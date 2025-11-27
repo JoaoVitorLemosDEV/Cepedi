ffd453e2c9edbd4517fb4067dac05aad534969d8
João Vitor <vitorrlemos2004@gmail.com>
2025-11-27 11:58:27 -0300

    Add initial HTML files and Tailwind CSS integration
    - Created index.html with a responsive layout and Tailwind CSS styling.
    - Added newsletter.html for a newsletter signup form with custom colors and fonts.
    - Included logo image for branding in index.html.
    - Configured Tailwind CSS theme with custom colors and font family in newsletter.html.

--- REACT_1.0/src/componentes/CardRecomenda/index.js
+++ REACT_1.0/src/componentes/CardRecomenda/index.js
@@ -2,16 +2,20 @@ import './estilo.css';
 import Titulo from '../Titulo';
 import Subtitulo from '../Subtitulo'; 
 import Paragrafo from '../Paragrafo';
-import livro2 from '../../imagens/livro2.jpeg';
+import { livro } from './dadosCardRecomenda';   
 
 function CardRecomenda() {
     return(
         <div className='cardRecomenda'>
-            <Titulo>Talvez você se interesse por</Titulo>
-            <Subtitulo>Angular 11</Subtitulo>
-            <Paragrafo>Construindo uma aplicação com a plataforma Google</Paragrafo>
-            <img src = '' alt='livro'/>
-            <button>Saiba mais</button>
+            <div className='texto'>
+                <Titulo>Talvez você se interesse por</Titulo>
+                <Subtitulo>Angular 11</Subtitulo>
+                <Paragrafo>Construindo uma aplicação com a plataforma Google</Paragrafo>
+            </div>
+            <div className='imagem'>
+                <img src = {livro.src} alt='livro'/>
+                <button>Saiba mais</button>
+            </div>
         </div>
     )   
 }
